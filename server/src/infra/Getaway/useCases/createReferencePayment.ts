ee544320ab4fe0fa65be814e1011a64bc26f3be0
Francisco Diakomas <franciscodiakoma@gmail.com>
2026-01-22 19:06:20 +0100

    feat

--- server/src/infra/Getaway/useCases/createReferencePayment.ts
+++ server/src/infra/Getaway/useCases/createReferencePayment.ts
@@ -140,6 +140,6 @@ export default class ReferencePayment implements PaymentStrategie {
   }
 
   private genIdentifier() {
-    return `PLIQPAG-${Date.now()}`;
+    return `PLIQPAG_API_${Date.now()}`;
   }
 }

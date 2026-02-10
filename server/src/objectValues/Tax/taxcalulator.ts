59a78d3d3283f211dddb5782bc1718c7b090f919
Francisco Diakomas <franciscodiakoma@gmail.com>
2026-02-10 20:31:39 +0100

    Update taxcalulator.ts

--- server/src/objectValues/Tax/taxcalulator.ts
+++ server/src/objectValues/Tax/taxcalulator.ts
@@ -10,37 +10,36 @@ export class TaxCalculatorFactory {
   constructor(private readonly taxType: TaxType) {}
 
   public calc(amount: number): ITaxCalculator {
-  if (amount <= 0) {
-    throw new BadRequestException(
-      'O valor da transação deve ser maior que zero',
-    );
-  }
+    if (amount <= 0) {
+      throw new BadRequestException(
+        'O valor da transação deve ser maior que zero',
+      );
+    }
 
-  if (amount > 10_000) {
-    throw new BadRequestException(
-      'Limite de 10.000,00 kz por transação',
-    );
-  }
+    if (amount > 10_000) {
+      throw new BadRequestException(
+        'Limite de 10.000,00 kz por transação',
+      );
+    }
 
-  const TAXA = 0.03;
-  const taxPercent = TAXA * 100;     
-  const taxValue = data.amount * TAXA;
-  const subtotal = data.amount - taxValue;
-  const total = subtotal; 
+    const TAXA = 0.03;               
+    const taxPercent = TAXA * 100;  
+    const subtotal = amount - taxValue;
+    const total = amount;       
 
-  if (total <= 0) {
-    throw new BadRequestException(
-      'O valor final não pode ser menor ou igual a zero',
-    );
-  }
+    if (total <= 0) {
+      throw new BadRequestException(
+        'O valor final não pode ser menor ou igual a zero',
+      );
+    }
 
-  return {
-      amount,
-      tax : taxPercent, 
-      subtotal,
-      total : amount,
+    return {
+      tax : taxPercent,        
+      subtotal, 
+      total,
       taxtType: 'PERCENT',
-  };
-}
+    };
+  }
+  
 
 }

65e72b1f7c2ee8af0149db0597f550fc5770d2b1
Francisco Diakomas <franciscodiakoma@gmail.com>
2026-03-26 15:00:14 +0100

    Disable transaction creation logic temporarily
    Comment out the transaction creation logic in create method.

--- server/src/domains/transactions/transaction.service.ts
+++ server/src/domains/transactions/transaction.service.ts
@@ -25,7 +25,8 @@ export default class TrasanctionService {
   ) {}
 
   public async create(data: CreateTransactionDTO, publickApiKey: string) {
-    const processor = new Paymentprocessor(data.method);
+    return null;
+    /* const processor = new Paymentprocessor(data.method);
     const paymentUseCase = new CreatePaymentUseCase(
       this.transactionRepo,
       processor,
@@ -37,7 +38,7 @@ export default class TrasanctionService {
     const { data: createPayment } = await paymentUseCase.pay(data, apikey);
     return {
       data: createPayment,
-    };
+    }; */
   }
 
   public async getByTenant(page: number, tenantId: string) {

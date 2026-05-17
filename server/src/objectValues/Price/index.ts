fffbb780834c2d0d2a62a8eccca2e9fc4f6574d0
Francisco Diakomas <franciscodiakoma@gmail.com>
2026-05-17 22:07:05 +0100

    Update index.ts

--- server/src/objectValues/Price/index.ts
+++ server/src/objectValues/Price/index.ts
@@ -7,29 +7,23 @@ export class Price {
   ) {
     this.validate();
   }
+  
   private validate(): void {
-    
-    if (this.price > 0) {
-      throw new BadRequestException({
-        message: 'Banco não Suportado || Internal server Error',
-      });
-    }
-    
     if (this.price <= 0) {
       throw new BadRequestException({
         message: 'Preço não pode ser negativo ou neutro',
       });
     }
   }
-
   public getValue(): number {
     return this.price * this.quantity;
   }
 }
 
+
 export class PriceValidator {
   private totalAmount: number = 0;
-
+  
   constructor(
     private readonly prices: Price[],
     private readonly wishisgValue: number,
@@ -38,6 +32,7 @@ export class PriceValidator {
   }
 
   private validate(): void {
+    
     this.prices.forEach((item) => {
       this.totalAmount += item.getValue();
     });
